a49109739f1ddcaa7cd262a79260852e28caa556
Aemon Cannon <aemoncannon@gmail.com>
2011-03-08 14:02:31 -0800

    Add all.

---
+++ src/org/stanford/mobisocial/dungbeetle/DungBeetleActivity.java
@@ -0,0 +1,15 @@
+package org.stanford.mobisocial.dungbeetle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DungBeetleActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
