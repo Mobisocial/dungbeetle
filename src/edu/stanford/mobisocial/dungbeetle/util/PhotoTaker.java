b760ef3127f73924d9226d572f1a2aa6940f9d92
Ben Dodson <bjdodson@handdog.(none)>
2011-04-21 01:38:06 -0700

    autoformatting

--- src/edu/stanford/mobisocial/dungbeetle/util/PhotoTaker.java
+++ src/edu/stanford/mobisocial/dungbeetle/util/PhotoTaker.java
@@ -22,84 +22,90 @@ import edu.stanford.mobisocial.dungbeetle.model.Object;
 import android.util.Base64;
 
 public class PhotoTaker implements ActivityCallout {
-		private final ResultHandler mResultHandler;
-		private final Context mContext;
+	private final ResultHandler mResultHandler;
+	private final Context mContext;
 
-		public PhotoTaker(Context c, ResultHandler handler) {
-			mContext = c;
-			mResultHandler = handler;
+	public PhotoTaker(Context c, ResultHandler handler) {
+		mContext = c;
+		mResultHandler = handler;
+	}
+
+	@Override
+	public Intent getStartIntent() {
+		final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		intent.putExtra(MediaStore.EXTRA_OUTPUT,
+				Uri.fromFile(getTempFile(mContext)));
+		return intent;
+	}
+
+	@Override
+	public void handleResult(int resultCode, Intent resultData) {
+		if (resultCode != Activity.RESULT_OK) {
+			return;
 		}
-    	@Override
-    	public Intent getStartIntent() {
-    		final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(getTempFile(mContext)) ); 
-            return intent;
-    	}
-    	
-    	@Override
-    	public void handleResult(int resultCode, Intent resultData) {
-    		if (resultCode != Activity.RESULT_OK) {
-    			return;
-    		}
 
-    		final File file;
-        	final File path = new File( Environment.getExternalStorageDirectory(), mContext.getPackageName() );
-            if(!path.exists()){
-                path.mkdir();
-            }
-            file = new File(path, "image.tmp");
-            try {
-                BitmapFactory.Options options=new BitmapFactory.Options();
-                options.inSampleSize = 8;
-                Bitmap sourceBitmap=BitmapFactory.decodeFile(file.getPath(),options);
+		final File file;
+		final File path = new File(Environment.getExternalStorageDirectory(),
+				mContext.getPackageName());
+		if (!path.exists()) {
+			path.mkdir();
+		}
+		file = new File(path, "image.tmp");
+		try {
+			BitmapFactory.Options options = new BitmapFactory.Options();
+			options.inSampleSize = 8;
+			Bitmap sourceBitmap = BitmapFactory.decodeFile(file.getPath(),
+					options);
 
-                
-                //Bitmap sourceBitmap = Media.getBitmap(getContentResolver(), Uri.fromFile(file) );
-                int width = sourceBitmap.getWidth();
-                int height = sourceBitmap.getHeight();
-                int cropSize = Math.min(width, height);
-                Bitmap cropped = Bitmap.createBitmap(sourceBitmap, 0, 0, cropSize, cropSize);
+			// Bitmap sourceBitmap = Media.getBitmap(getContentResolver(),
+			// Uri.fromFile(file) );
+			int width = sourceBitmap.getWidth();
+			int height = sourceBitmap.getHeight();
+			int cropSize = Math.min(width, height);
+			Bitmap cropped = Bitmap.createBitmap(sourceBitmap, 0, 0, cropSize,
+					cropSize);
 
-                int targetSize = 80;
-                float scaleSize = ((float) targetSize) / cropSize;
-                Matrix matrix = new Matrix();
-                // resize the bit map
-                matrix.postScale(scaleSize, scaleSize);
-                matrix.postRotate(270);
+			int targetSize = 80;
+			float scaleSize = ((float) targetSize) / cropSize;
+			Matrix matrix = new Matrix();
+			// resize the bit map
+			matrix.postScale(scaleSize, scaleSize);
+			matrix.postRotate(270);
 
-                // recreate the new Bitmap
-                Bitmap resizedBitmap = Bitmap.createBitmap(cropped, 0, 0, 
-                                  cropSize, cropSize, matrix, true);
-                
-                final ImageView icon = new ImageView(mContext);
-                icon.setImageBitmap(resizedBitmap);
+			// recreate the new Bitmap
+			Bitmap resizedBitmap = Bitmap.createBitmap(cropped, 0, 0, cropSize,
+					cropSize, matrix, true);
 
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();  
-                resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos); //bm is the bitmap object   
-                byte[] data = baos.toByteArray(); 
+			final ImageView icon = new ImageView(mContext);
+			icon.setImageBitmap(resizedBitmap);
 
-                ContentValues values = new ContentValues();
-                String encoded = Base64.encodeToString(data, Base64.DEFAULT);
-                JSONObject obj = ProfilePictureObj.json(encoded);
-                values.put(Object.JSON, obj.toString());
-                values.put(Object.TYPE, ProfilePictureObj.TYPE);
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+			byte[] data = baos.toByteArray();
 
-                mResultHandler.onResult(values);
-            } catch (Exception e) {
-            	
-            }
-    	}
-    	
-    	public interface ResultHandler {
-    		public void onResult(ContentValues values);
-    	}
-    	
-    	private static File getTempFile(Context context){
-            //it will return /sdcard/image.tmp
-            final File path = new File( Environment.getExternalStorageDirectory(), context.getPackageName() );
-            if(!path.exists()){
-                path.mkdir();
-            }
-            return new File(path, "image.tmp");
-        }
-    }
+			ContentValues values = new ContentValues();
+			String encoded = Base64.encodeToString(data, Base64.DEFAULT);
+			JSONObject obj = ProfilePictureObj.json(encoded);
+			values.put(Object.JSON, obj.toString());
+			values.put(Object.TYPE, ProfilePictureObj.TYPE);
+
+			mResultHandler.onResult(values);
+		} catch (Exception e) {
+
+		}
+	}
+
+	public interface ResultHandler {
+		public void onResult(ContentValues values);
+	}
+
+	private static File getTempFile(Context context) {
+		// it will return /sdcard/image.tmp
+		final File path = new File(Environment.getExternalStorageDirectory(),
+				context.getPackageName());
+		if (!path.exists()) {
+			path.mkdir();
+		}
+		return new File(path, "image.tmp");
+	}
+}
