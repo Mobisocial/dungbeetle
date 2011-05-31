3e3a574f4a9f5ad9d9e5386d606efa0d71edd31a
Ben Dodson <bjdodson@handdog.(none)>
2011-05-30 17:46:38 -0700

    slimming down Helpers class

--- src/edu/stanford/mobisocial/dungbeetle/Helpers.java
+++ src/edu/stanford/mobisocial/dungbeetle/Helpers.java
@@ -1,5 +1,5 @@
 package edu.stanford.mobisocial.dungbeetle;
-import android.util.Log;
+
 import edu.stanford.mobisocial.dungbeetle.model.MyInfo;
 import edu.stanford.mobisocial.dungbeetle.util.Util;
 import edu.stanford.mobisocial.dungbeetle.model.Group;
@@ -16,9 +16,6 @@ import android.content.ContentValues;
 import android.net.Uri;
 import edu.stanford.mobisocial.dungbeetle.model.Contact;
 import android.content.Context;
-import android.database.Cursor;
-import com.skjegstad.utils.BloomFilter;
-import java.util.ArrayList;
 
 public class Helpers {
     public static final String TAG = "Helpers";
@@ -84,54 +81,6 @@ public class Helpers {
         values.put(GroupMember.CONTACT_ID, contactId);
         values.put(GroupMember.GLOBAL_CONTACT_ID, idInGroup);
         c.getContentResolver().insert(url, values);
-    }
-
-    public static BloomFilter getFriendsBloomFilter(final Context c) {
-        BloomFilter<String> friendsFilter = new BloomFilter<String>(.001, 1000);
-        Cursor cursor = c.getContentResolver().query(
-            Uri.parse(DungBeetleContentProvider.CONTENT_URI + "/contacts"), 
-            new String[]{Contact.PUBLIC_KEY}, 
-            null, 
-            null, 
-            null);
-
-        cursor.moveToFirst();
-        while(!cursor.isAfterLast()){
-            String publicKey = cursor.getString(cursor.getColumnIndexOrThrow(Contact.PUBLIC_KEY));
-            friendsFilter.add(publicKey);
-            cursor.moveToNext();
-        }
-
-        return friendsFilter;    
-    }
-
-    public static Contact[] checkFriends(final Context c, BloomFilter friendsFilter) {
-        Cursor cursor = c.getContentResolver().query(
-            Uri.parse(DungBeetleContentProvider.CONTENT_URI + "/contacts"), 
-            null, 
-            null, 
-            null, 
-            null);
-
-        cursor.moveToFirst();
-
-        ArrayList<Contact> friends = new ArrayList<Contact>();
-        while(!cursor.isAfterLast()){
-            Contact contact = new Contact(cursor);
-            String publicKey = cursor.getString(cursor.getColumnIndexOrThrow(Contact.PUBLIC_KEY));
-            String name = cursor.getString(cursor.getColumnIndexOrThrow(Contact.NAME));
-            if(friendsFilter.contains(publicKey)) {
-                Log.w("bloomfilter", name + " is a friend");
-                friends.add(contact);
-            }
-            else {
-                Log.w("bloomfilter", name + " is not a friend");
-            }
-            cursor.moveToNext();
-        }
-
-        Contact[] friendsArray = new Contact[friends.size()];
-        return friends.toArray(friendsArray);
     }
 
     public static void sendIM(final Context c, 
