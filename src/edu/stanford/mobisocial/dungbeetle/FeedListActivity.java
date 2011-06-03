1c590ac5adee150bdd7ee20d472b58d3ccb55d94
Ben Dodson <bjdodson@handdog.(none)>
2011-06-03 01:50:33 -0700

    added color for feed name

--- src/edu/stanford/mobisocial/dungbeetle/FeedListActivity.java
+++ src/edu/stanford/mobisocial/dungbeetle/FeedListActivity.java
@@ -11,6 +11,7 @@ import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -89,6 +90,8 @@ public class FeedListActivity extends ListActivity {
             } else {
                 labelView.setText(feedName);
             }
+            
+            labelView.setBackgroundColor(Feed.colorFor(feedName));
         }
     }
 
