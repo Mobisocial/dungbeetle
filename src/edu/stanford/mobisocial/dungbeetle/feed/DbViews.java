6d3267d3200d841795f277fbd1ec296d3452fb2e
Ben Dodson <bjdodson@handdog.(none)>
2011-08-12 12:14:53 -0700

    mockup for graphing gps on a map

--- src/edu/stanford/mobisocial/dungbeetle/feed/DbViews.java
+++ src/edu/stanford/mobisocial/dungbeetle/feed/DbViews.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import edu.stanford.mobisocial.dungbeetle.feed.iface.FeedView;
+import edu.stanford.mobisocial.dungbeetle.feed.view.MapView;
 import edu.stanford.mobisocial.dungbeetle.feed.view.StatView;
 
 public class DbViews {
     private static final List<FeedView> sFeedViews = new ArrayList<FeedView>();
     static {
         sFeedViews.add(new FeedView("Stats", StatView.class));
+        sFeedViews.add(new FeedView("Map", MapView.class));
     }
 
     public static List<FeedView> getFeedViews() {
