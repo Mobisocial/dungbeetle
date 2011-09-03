4150a6656b77dce1ba03e54d92367a6ead2ab43c
Ben Dodson <bjdodson@gmail.com>
2011-09-02 17:53:10 -0700

    deprecated Publisher; use content provider / SocialKit.

--- src/edu/stanford/mobisocial/dungbeetle/Publisher.java
+++ src/edu/stanford/mobisocial/dungbeetle/Publisher.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.net.Uri;
 import edu.stanford.mobisocial.dungbeetle.model.AppState;
 
+@Deprecated
 public class Publisher extends BroadcastReceiver {
 
     @Override
