1bd4d19dda782d6cd2a89ec4818d9d0720ecc1e9
Ben Dodson <bjdodson@gmail.com>
2011-09-05 15:21:35 -0700

    set profile from image

--- src/edu/stanford/mobisocial/dungbeetle/ImageViewerActivity.java
+++ src/edu/stanford/mobisocial/dungbeetle/ImageViewerActivity.java
@@ -1,6 +1,7 @@
 package edu.stanford.mobisocial.dungbeetle;
 import android.app.Activity;
 import android.net.Uri;
+import edu.stanford.mobisocial.dungbeetle.util.Base64;
 import edu.stanford.mobisocial.dungbeetle.util.BitmapManager;
 import android.content.Intent;
 import android.os.Bundle;
@@ -29,22 +30,23 @@ public class ImageViewerActivity extends Activity{
 
 	private Bitmap bitmap;
 	private String extStorageDirectory;
+	private Intent mIntent;
 
     public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.image_viewer);
 		im = (ImageView)findViewById(R.id.image);
 		im.setScaleType(ImageView.ScaleType.FIT_CENTER);
-		Intent intent = getIntent();
+		mIntent = getIntent();
 
-        if(intent.hasExtra("image_url")){
-            String url = intent.getStringExtra("image_url");
+        if(mIntent.hasExtra("image_url")){
+            String url = mIntent.getStringExtra("image_url");
             ((App)getApplication()).objectImages.lazyLoadImage(
                 url.hashCode(), Uri.parse(url), im);
             bitmap = mgr.getBitmap(url.hashCode(), url);
         }
-        else if(intent.hasExtra("b64Bytes")){
-            String b64Bytes = intent.getStringExtra("b64Bytes");
+        else if(mIntent.hasExtra("b64Bytes")){
+            String b64Bytes = mIntent.getStringExtra("b64Bytes");
             ((App)getApplication()).objectImages.lazyLoadImage(
                 b64Bytes.hashCode(), b64Bytes, im);
             bitmap = mgr.getBitmapB64(b64Bytes.hashCode(), b64Bytes);
@@ -57,43 +59,62 @@ public class ImageViewerActivity extends Activity{
 
 	
     private final static int SAVE = 0;
+    private final static int SET_PROFILE = 1;
 
     public boolean onPreparePanel(int featureId, View view, Menu menu) {
         menu.clear();
-        menu.add(0, SAVE, 0, "Download Picture to SD Card");
+        menu.add(0, SAVE, 0, "Download to SD Card");
+        menu.add(0, SET_PROFILE, 0, "Set as Profile");
         //menu.add(1, ANON, 1, "Add anon profile");
         return true;
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()){
-        case SAVE: {
-            OutputStream outStream = null;
-            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
-            Date date = new Date();
-            File file = new File(extStorageDirectory, dateFormat.format(date) + ".PNG");
-            File fileDirectory = new File(extStorageDirectory);
-            fileDirectory.mkdirs();
-            try {
-                outStream = new FileOutputStream(file);
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream);
-                outStream.flush();
-                outStream.close();
+        switch (item.getItemId()) {
+            case SAVE: {
+                OutputStream outStream = null;
+                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+                Date date = new Date();
+                File file = new File(extStorageDirectory, dateFormat.format(date) + ".PNG");
+                File fileDirectory = new File(extStorageDirectory);
+                fileDirectory.mkdirs();
+                try {
+                    outStream = new FileOutputStream(file);
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream);
+                    outStream.flush();
+                    outStream.close();
 
-                Toast.makeText(ImageViewerActivity.this, "Saved", Toast.LENGTH_LONG).show();
+                    Toast.makeText(ImageViewerActivity.this, "Saved", Toast.LENGTH_LONG).show();
 
-            } catch (FileNotFoundException e) {
-    // TODO Auto-generated catch block
-    e.printStackTrace();
-    Toast.makeText(ImageViewerActivity.this, e.toString(), Toast.LENGTH_LONG).show();
-   } catch (IOException e) {
-    // TODO Auto-generated catch block
-    e.printStackTrace();
-    Toast.makeText(ImageViewerActivity.this, e.toString(), Toast.LENGTH_LONG).show();
-   }
-            return true;
-        }
-        default: return false;
+                } catch (FileNotFoundException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                    Toast.makeText(ImageViewerActivity.this, e.toString(), Toast.LENGTH_LONG)
+                            .show();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                    Toast.makeText(ImageViewerActivity.this, e.toString(), Toast.LENGTH_LONG)
+                            .show();
+                }
+                return true;
+            }
+            case SET_PROFILE: {
+                if(mIntent.hasExtra("b64Bytes")) {
+                    String b64Bytes = mIntent.getStringExtra("b64Bytes");
+                    byte[] data = Base64.decode(b64Bytes);
+                    Helpers.updatePicture(ImageViewerActivity.this, data);
+                    Toast.makeText(ImageViewerActivity.this,
+                            "Set profile picture.", Toast.LENGTH_LONG).show(); 
+                    
+                } else {
+                    Toast.makeText(ImageViewerActivity.this,
+                            "Error setting profile picture.", Toast.LENGTH_LONG).show();
+                }
+                return true;
+            }
+            default:
+                return false;
         }
     }
 
@@ -104,6 +125,3 @@ public class ImageViewerActivity extends Activity{
 	}
 
 }
-
-
-
---
+++ src/edu/stanford/mobisocial/dungbeetle/obj/action/SetProfileObjAction.java
@@ -0,0 +1,35 @@
+
+package edu.stanford.mobisocial.dungbeetle.obj.action;
+
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.widget.Toast;
+import edu.stanford.mobisocial.dungbeetle.Helpers;
+import edu.stanford.mobisocial.dungbeetle.feed.iface.DbEntryHandler;
+import edu.stanford.mobisocial.dungbeetle.feed.objects.PictureObj;
+import edu.stanford.mobisocial.dungbeetle.obj.iface.ObjAction;
+import edu.stanford.mobisocial.dungbeetle.ui.DashboardBaseActivity;
+import edu.stanford.mobisocial.dungbeetle.util.Base64;
+
+public class SetProfileObjAction extends ObjAction {
+    public void onAct(Context context, DbEntryHandler objType, JSONObject objData) {
+        String b64Bytes = objData.optString(PictureObj.DATA);
+        byte[] data = Base64.decode(b64Bytes);
+        Helpers.updatePicture(context, data);
+        Toast.makeText(context, "Set profile picture.", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public String getLabel() {
+        return "Set as Profile";
+    }
+
+    @Override
+    public boolean isActive(DbEntryHandler objType, JSONObject objData) {
+        if (!DashboardBaseActivity.getInstance().isDeveloperModeEnabled()) {
+            return false;
+        }
+        return (objType instanceof PictureObj);
+    }
+}
